d16e3098f9daa163ad0e4e8b99c4ee9f6d68e8b0
Gwyn Price <gwyn@gwynprice.com>
2015-04-03 15:59:55 -0700

    added heroku stuff

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -81,6 +81,15 @@ DATABASES = {
     }
 }
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -106,7 +115,7 @@ TEMPLATE_DIRS = (
  )
 
 STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
+ os.path.join(MAIN_DIR, 'static'),
  )
 
 STATIC_ROOT = 'staticfiles'
